4eb106fd22dfe4725ebcf500458edbafff82c06e
BtcLayer <Sainath@btclayer.org>
2024-02-26 19:10:15 +0530

    Recursive proving kinda works

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
-# BitVM Toy Implementation
+# Plonky2 Experiments
+Trying out recursive circuits in plonky2 for zkNano/zkCoins
 
-A work-in-progress toy-implementation of BitVM. This is still extremely experimental code which's purpose is mostly for us to figure out how to build BitVM. This project is only meant to be a draft or blueprint for a proper implementation in Rust.
-
-That's why it is totally messy and not documented. We will clean this up asap.
-
-
-
-## Development 
-
-Serve the root directory with a local web server. For example, Python3 `python3 -m http.server 7777` or Python2: `python -m SimpleHTTPServer 7777`. Then open http://localhost:7777/run
-
-## Bitcoin Script Interpreter
-
-Our Bitcoin Script [interpreter](https://bitvm.github.io/BitVM/run/interpreter.html) is a tool to develop complex Bitcoin Scripts.
+# Useful Resources
+- Simple example: https://polymerlabs.medium.com/a-tutorial-on-writing-zk-proofs-with-plonky2-part-i-be5812f6b798
+- Hash chain IVC example: https://docs.rs/plonky2/latest/src/plonky2/recursion/cyclic_recursion.rs.html#136-155
+- Plonky2 Repo: https://github.com/mir-protocol/plonky2/tree/main
--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,182 @@
-use std::fmt;
-use std::io::{self, Write};
-use std::path::PathBuf;
+use anyhow::Result;
+use plonky2::field::extension::Extendable;
+use plonky2::gates::constant::ConstantGate;
+use plonky2::gates::gate::GateRef;
+use plonky2::field::types::Field;
+use plonky2::gates::noop::NoopGate;
+use plonky2::hash::hash_types::RichField;
+use plonky2::iop::witness::{PartialWitness, WitnessWrite};
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+use plonky2::plonk::circuit_data::{CircuitConfig, CommonCircuitData};
+use plonky2::plonk::config::{AlgebraicHasher, GenericConfig, PoseidonGoldilocksConfig};
+use plonky2::recursion::cyclic_recursion::check_cyclic_proof_verifier_data;
+use plonky2::recursion::dummy_circuit::cyclic_base_proof;
+use std::time::SystemTime;
 
-use bitcoin::hashes::Hash;
-use bitcoin::hex::DisplayHex;
-use bitcoin::taproot::TapLeafHash;
-use bitcoin::{ScriptBuf, Transaction};
-use clap::Parser;
+fn common_data_for_recursion<
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+>() -> CommonCircuitData<F, D>
+where
+    C::Hasher: AlgebraicHasher<F>,
+{
+    let config = CircuitConfig::standard_recursion_config();
+    let builder = CircuitBuilder::<F, D>::new(config);
+    let data = builder.build::<C>();
+    let config = CircuitConfig::standard_recursion_config();
+    let mut builder = CircuitBuilder::<F, D>::new(config);
+    let proof = builder.add_virtual_proof_with_pis(&data.common);
+    let verifier_data = builder.add_virtual_verifier_data(data.common.config.fri_config.cap_height);
+    builder.verify_proof::<C>(&proof, &verifier_data, &data.common);
+    builder.verify_proof::<C>(&proof, &verifier_data, &data.common);
+    let data = builder.build::<C>();
 
-use bitcoin_scriptexec::*;
-
-#[derive(Parser)]
-#[command(author = "Steven Roose <steven@roose.io>", version, about)]
-struct Args {
-    /// filepath to script ASM file
-    #[arg(required = true)]
-    script_path: PathBuf,
-    /// Whether to print debug info
-    #[arg(long)]
-    debug: bool,
-    /// Whether to output result in JSON.
-    #[arg(long)]
-    json: bool,
+    let config = CircuitConfig::standard_recursion_config();
+    let num_consts = config.num_constants;
+    let mut builder = CircuitBuilder::<F, D>::new(config);
+    let proof = builder.add_virtual_proof_with_pis(&data.common);
+    let verifier_data = builder.add_virtual_verifier_data(data.common.config.fri_config.cap_height);
+    builder.verify_proof::<C>(&proof, &verifier_data, &data.common);
+    builder.verify_proof::<C>(&proof, &verifier_data, &data.common);
+    builder.verify_proof::<C>(&proof, &verifier_data, &data.common);
+    builder.add_gate_to_gate_set(GateRef::new(ConstantGate::new(num_consts)));
+    builder.build::<C>().common
 }
 
-/// A wrapper for the stack types to print them better.
-struct FmtStack<'a>(&'a Stack);
-impl<'a> fmt::Display for FmtStack<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut iter = self.0.iter_str().rev().peekable();
-        while let Some(item) = iter.next() {
-            write!(f, "<{}>", item.as_hex())?;
-            if iter.peek().is_some() {
-                write!(f, " ")?;
-            }
-        }
-        Ok(())
-    }
-}
+// Example Sum for PCD/IVC (output = previous_output + other_proof_output),
+// The previous proof and other proof are supposed to be the same IVC circuit.
+// Layout:
+// - initial value
+// - output value
+//
+// Condition:
+// - verify_proofs (boolean)
+fn main() -> Result<()> {
+    const D: usize = 2;
+    type C = PoseidonGoldilocksConfig;
+    type F = <C as GenericConfig<D>>::F;
 
-fn inner_main() -> Result<(), String> {
-    let args = Args::parse();
+    let check0 = SystemTime::now();
 
-    let script_asm = std::fs::read_to_string(args.script_path).expect("error reading script file");
-    let script = ScriptBuf::parse_asm(&script_asm).expect("error parsing script");
-    println!("Script in hex: {}", script.as_bytes().to_lower_hex_string());
-    println!("Script size: {} bytes", script.as_bytes().len());
+    let config = CircuitConfig::standard_recursion_config();
+    let mut builder = CircuitBuilder::<F, D>::new(config);
 
-    let start = std::time::Instant::now();
-    let mut exec = Exec::new(
-        ExecCtx::Tapscript,
-        Options::default(),
-        TxTemplate {
-            tx: Transaction {
-                version: bitcoin::transaction::Version::TWO,
-                lock_time: bitcoin::locktime::absolute::LockTime::ZERO,
-                input: vec![],
-                output: vec![],
-            },
-            prevouts: vec![],
-            input_idx: 0,
-            taproot_annex_scriptleaf: Some((TapLeafHash::all_zeros(), None)),
-        },
-        script,
-        vec![],
-    )
-    .expect("error creating exec");
+    // Public inputs
+    let initial_value = builder.add_virtual_public_input();
+    let output_value = builder.add_virtual_public_input();
 
-    const SEP: &str = "--------------------------------------------------";
+    // TODO: Is the return value supposed to be unused?
+    let verifier_data_target = builder.add_verifier_data_public_inputs();
+    
+    let mut common_data = common_data_for_recursion::<F, C, D>();
+    common_data.num_public_inputs = builder.num_public_inputs();
+    
+    let verify_proofs = builder.add_virtual_bool_target_safe();
 
-    let mut out = io::stdout();
-    println!("{}", SEP);
-    loop {
-        if args.debug {
-            if args.json {
-                let step = json::RunStep {
-                    remaining_script: exec.remaining_script(),
-                    stack: &exec.stack().iter_str().collect::<Vec<Vec<u8>>>(),
-                    altstack: &exec.altstack().iter_str().collect::<Vec<Vec<u8>>>(),
-                    stats: Some(exec.stats()),
-                };
-                serde_json::to_writer(&out, &step).expect("I/O error");
-                out.write_all(&['\n' as u8]).expect("I/O error");
-            } else {
-                println!(
-                    "Remaining script: {}",
-                    exec.remaining_script().to_asm_string()
-                );
-                println!("Stack: {}", FmtStack(&exec.stack()));
-                println!("AltStack: {}", FmtStack(&exec.altstack()));
-                println!("{}", SEP);
-            }
-        }
+    // Unpack inner proof's public inputs.
+    let inner_cyclic_proof_with_pis = builder.add_virtual_proof_with_pis(&common_data);
+    let inner_cyclic_pis = &inner_cyclic_proof_with_pis.public_inputs;
+    let inner_cyclic_initial_value = inner_cyclic_pis[0];
+    let inner_cyclic_output_value = inner_cyclic_pis[1];
 
-        if exec.exec_next().is_err() {
-            break;
-        }
-    }
+    // Unpack other proof's public inputs.
+    let other_proof_with_pis = builder.add_virtual_proof_with_pis(&common_data);
+    let other_pis = &other_proof_with_pis.public_inputs;
+    let _other_initial_value = other_pis[0];
+    let other_output_value = other_pis[1];
+    
+    // Connect our initial value to that of our inner proof.
+    builder.connect(initial_value, inner_cyclic_initial_value);
 
-    let res = exec.result().unwrap().clone();
-    if args.json {
-        let ret = json::RunResult {
-            success: res.success,
-            error: res.error.map(|e| format!("{:?}", e)), //TODO(stevenroose) fmt::Display
-            opcode: res.opcode,
-            final_stack: &res.final_stack.iter_str().collect::<Vec<Vec<u8>>>(),
-            stats: Some(exec.stats()),
-        };
-        serde_json::to_writer(&out, &ret).expect("I/O error");
-    } else {
-        println!("Execution ended. Success: {}", res.success);
-        print!("Final stack: {}", FmtStack(&res.final_stack));
-        println!();
-        if !res.success {
-            println!("Failed on opcode: {:?}", res.opcode);
-            println!("Error: {:?}", res.error);
-        }
-        println!("Stats:\n{:#?}", exec.stats());
-        println!("Time elapsed: {}ms", start.elapsed().as_millis());
-    }
-    return Ok(());
-}
+    // The input value is the previous output value if we have an inner proof, or the initial
+    // value if this is the base case.
+    // Initial value could be constrained to be 0.
+    let actual_value_in = builder.select(verify_proofs, inner_cyclic_output_value, initial_value);
 
-fn main() {
-    if let Err(e) = inner_main() {
-        eprintln!("ERROR: {}", e);
-    }
+    let new_output_value = builder.mul_add(verify_proofs.target, other_output_value, actual_value_in);
+    builder.connect(output_value, new_output_value);
+    
+    // Verify inner proof
+    builder.conditionally_verify_cyclic_proof_or_dummy::<C>(
+        verify_proofs,
+        &inner_cyclic_proof_with_pis,
+        &common_data,
+    )?;
+
+    // Verify other proof
+    builder.conditionally_verify_cyclic_proof_or_dummy::<C>(
+        verify_proofs,
+        &other_proof_with_pis,
+        &common_data,
+    )?;
+
+    // END OF THE CIRCUIT
+
+    // TODO: Generate dummy proof with cyclic_base_proof
+    // https://docs.rs/plonky2/0.1.4/plonky2/recursion/dummy_circuit/fn.cyclic_base_proof.html
+
+    let cyclic_circuit_data = builder.build::<C>();
+    let check1 = SystemTime::now();
+    println!("Constructed Circuit Builder {}", check1.duration_since(check0).unwrap().as_secs());
+
+    // Construct Base Proof
+    let mut pw = PartialWitness::new();
+    let initial_targets = [F::ZERO];
+    let initial_targets_pis = initial_targets.into_iter().enumerate().collect();
+    
+    let initial_other_targets = [F::ZERO];
+    let initial_other_targets_pis = initial_targets.into_iter().enumerate().collect();
+
+    pw.set_bool_target(verify_proofs, false);
+    pw.set_proof_with_pis_target::<C, D>(
+        &inner_cyclic_proof_with_pis,
+        &cyclic_base_proof(
+            &common_data,
+            &cyclic_circuit_data.verifier_only,
+            initial_targets_pis,
+        ),
+    );
+    pw.set_proof_with_pis_target::<C, D>(
+        &other_proof_with_pis,
+        &cyclic_base_proof(
+            &common_data,
+            &cyclic_circuit_data.verifier_only,
+            initial_other_targets_pis,
+        ),
+    );
+    pw.set_verifier_data_target(&verifier_data_target, &cyclic_circuit_data.verifier_only);
+    let proof = cyclic_circuit_data.prove(pw)?;
+    let check2 = SystemTime::now();
+    println!("Constructed Proof, {}", check2.duration_since(check1).unwrap().as_secs());
+
+    check_cyclic_proof_verifier_data(
+        &proof,
+        &cyclic_circuit_data.verifier_only,
+        &cyclic_circuit_data.common,
+        )?;
+    cyclic_circuit_data.verify(proof.clone())?;
+    let check3 = SystemTime::now();
+    println!("Verified Base Proof, {}", check3.duration_since(check2).unwrap().as_secs());
+    
+    // Construct 1st Level Proof
+    let mut pw = PartialWitness::new();
+    pw.set_bool_target(verify_proofs, true);
+    pw.set_proof_with_pis_target::<C, D>(&inner_cyclic_proof_with_pis, &proof);
+    pw.set_proof_with_pis_target::<C, D>(&other_proof_with_pis, &proof);
+    pw.set_verifier_data_target(&verifier_data_target, &cyclic_circuit_data.verifier_only);
+    let proof = cyclic_circuit_data.prove(pw)?;
+    let check4 = SystemTime::now();
+    println!("Constructed Proof, {}", check4.duration_since(check3).unwrap().as_secs());
+
+    check_cyclic_proof_verifier_data(
+        &proof,
+        &cyclic_circuit_data.verifier_only,
+        &cyclic_circuit_data.common,
+        )?;
+    cyclic_circuit_data.verify(proof.clone())?;
+    let check5 = SystemTime::now();
+    println!("Verifier 1st Layer Proof, {}", check5.duration_since(check4).unwrap().as_secs());
+
+    Ok(())
 }
